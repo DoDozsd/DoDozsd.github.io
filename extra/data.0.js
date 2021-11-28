cb17402c1100560a4e0851e725914a1d788599ab
DoDozsd <93237897+DoDozsd@users.noreply.github.com>
2021-11-28 09:13:21 +0800

    Delete extra directory

--- extra/data.0.js
+++
@@ -1,1 +0,0 @@
-window["hzv3"]["pid"] = {"user":null,"locale":"zh-CN","preference":null,"projectAccessToken":"9fec634174d32a3cf247e9a36ec16ca5ef842816","projectEncryptedPassword":"","isPasswordRequired":true,"is_on_premises":false,"is_wonder_share":false};
