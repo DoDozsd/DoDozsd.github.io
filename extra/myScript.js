3065161c41c59aa6ae7f7c183935467ba8f64d66
DoDozsd <93237897+DoDozsd@users.noreply.github.com>
2021-12-02 15:48:46 +0800

    Add files via upload

---
+++ extra/myScript.js
@@ -0,0 +1,5 @@
+<button onclick="document.getElementById('myImage1').src='/i/eg_bulbon.gif'">开灯</button>
+
+<img id="myImage" border="0" src="/i/eg_bulboff.gif" style="text-align:center;">
+
+<button onclick="document.getElementById('myImage1').src='/i/eg_bulboff.gif'">关灯</button>
