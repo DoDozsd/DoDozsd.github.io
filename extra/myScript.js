841cf56dbfc9fe1b4ed7c2c7a8fbabc19e827154
DoDozsd <93237897+DoDozsd@users.noreply.github.com>
2021-12-03 15:02:40 +0800

    Delete extra directory

--- extra/myScript.js
+++
@@ -1,5 +0,0 @@
-<button onclick="document.getElementById('myImage1').src='/i/eg_bulbon.gif'">开灯</button>
-
-<img id="myImage" border="0" src="/i/eg_bulboff.gif" style="text-align:center;">
-
-<button onclick="document.getElementById('myImage1').src='/i/eg_bulboff.gif'">关灯</button>
